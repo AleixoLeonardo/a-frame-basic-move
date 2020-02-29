079249a7f41756345c15e6b5e0c406ff4b9a3c39
AleixoLeonardo <aleixo1leonardo@gmail.com>
2020-02-28 21:54:10 -0300

    exemplo com um carro mais o menos

--- index.js
+++ index.js
@@ -1,68 +1,66 @@
 
 
-function moveBoxToUp(){
-    var boxEl = document.getElementById("caixa-1");
+function moveBoxToUp() {
+    var boxEl = document.getElementById("roda_tras");
     var position = boxEl.getAttribute('position');
     position.y = position.y + 0.5;
     boxEl.setAttribute('position', position);
 }
 
-function moveBoxToRight(){
-    var boxEl = document.getElementById("caixa-1");
+function moveBoxToRight() {
+    var boxEl = document.getElementById("roda_tras");
     var position = boxEl.getAttribute('position');
     position.x = position.x - 0.5;
     boxEl.setAttribute('position', position);
 }
 
-function moveBoxToLeft(){
-    var boxEl = document.getElementById("caixa-1");
+function moveBoxToLeft() {
+    var boxEl = document.getElementById("roda_tras");
     var position = boxEl.getAttribute('position');
     position.x = position.x + 0.5;
     boxEl.setAttribute('position', position);
 }
 
-function moveZRight(){
-    var boxEl = document.getElementById("caixa-1");
+function moveZRight() {
+    var boxEl = document.getElementById("roda_tras");
     var position = boxEl.getAttribute('position');
     position.z = position.z + 0.5;
     boxEl.setAttribute('position', position);
 }
 
-function moveZLeft(){
-    var boxEl = document.getElementById("caixa-1");
+function moveZLeft() {
+    var boxEl = document.getElementById("roda_tras");
     var position = boxEl.getAttribute('position');
     position.z = position.z - 0.5;
     boxEl.setAttribute('position', position);
 }
 
-function moveBoxToDown(){
-    var boxEl = document.getElementById("caixa-1");
+function moveBoxToDown() {
+    var boxEl = document.getElementById("roda_tras");
     var position = boxEl.getAttribute('position');
     position.y = position.y - 0.5;
-    if (position.y >= 0.5){
+    if (position.y >= 0.5) {
         boxEl.setAttribute('position', position);
     }
 }
 
-function check(){
-    var pBox1 = document.getElementById("caixa-1").getAttribute("position");
-    var box1 = document.getElementById("caixa-1");
-    var pBox2 = document.getElementById("caixa-2").getAttribute("position");
-    if (compareData(pBox1.x, pBox2.x)
-        && compareData(pBox1.z, pBox2.z)
-            && (pBox2.y + 1) == pBox1.y){
-                box1.setAttribute("color", 'green');
-    }else{
-        box1.setAttribute("color", 'blue');
+function check() {
+    var pRoda = document.getElementById("roda_tras").getAttribute("position");
+    var roda = document.getElementById("roda_tras");
+    if (compareData(pRoda.x, 2) && compareData(pRoda.y, 1) && compareData(pRoda.z, -9)) {
+        roda.setAttribute("color", 'green');
+    } else {
+        roda.setAttribute("color", 'red');
     }
+
 }
 
 
 
-function compareData(d1, d2){
+function compareData(d1, d2) {
     return ("" + d1) == ("" + d2);
 }
-document.onkeydown = function(e) {
+document.onkeydown = function (e) {
     switch (e.keyCode) {
         case 100:
             moveBoxToRight();
